f0ece46d75b0d34fd122ee2818c8d8ce47be71e8
anna shcherbina <annashcherbina@gmail.com>
2020-04-25 16:18:16 -0700

    added get_gc_content.py locally

--- make_inputs/get_gc_positives.sh
+++ make_inputs/get_gc_positives.sh
@@ -3,7 +3,7 @@ for split in `seq 0 9`
 do
     for dataset in train test
     do
-	python /srv/scratch/annashch/5_cell_lines_bias_correction/genomewide_gc/get_gc_content.py \
+	python get_gc_content.py \
 	       --input_bed $task/svm.peaks.$task.$dataset.$split \
 	       --ref_fasta /mnt/data/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta \
 	       --out_prefix $task/svm.peaks.$task.$dataset.$split.gc.seq \
