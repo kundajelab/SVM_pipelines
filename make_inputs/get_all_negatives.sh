581ff5d544f7b8d79cb93dd40e82954bc9604301
anna shcherbina <annashcherbina@gmail.com>
2020-09-15 17:00:10 -0700

    clarified how to get genome gc

--- make_inputs/get_all_negatives.sh
+++ make_inputs/get_all_negatives.sh
@@ -1,4 +1,5 @@
 #get the inverse intersection of idr peak file and all gc genome bins
 task=$1
 idr=$2
-bedtools intersect -v -a /srv/scratch/annashch/5_cell_lines_bias_correction/genomewide_gc/gc_hg38_nosmooth.tsv -b $task/$idr > $task/$task.candidate.negatives.tsv
+genomewide_gc=$3
+bedtools intersect -v -a $genomewide_gc -b $task/$idr > $task/$task.candidate.negatives.tsv
