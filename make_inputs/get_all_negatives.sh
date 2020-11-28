4e227d2908f5377d7462fe829008eacbd7229803
soumyakundu <soumya.kundu@stanford.edu>
2020-11-27 16:55:36 -0800

    Fixed bugs in get_all_negatives.sh

--- make_inputs/get_all_negatives.sh
+++ make_inputs/get_all_negatives.sh
@@ -11,10 +11,12 @@ rm -f $outdir/$task/$task.all.positives.bed
 
 for split in `seq 0 9`
 do
-    cat $outdir/$task/svm.peaks.$task.test.$split.bed >> $outdir/$task/$task.all.positives.bed
+    cat $outdir/$task/svm.peaks.$task.test.$split.gc.seq | cut -f 1-3 >> $outdir/$task/$task.all.positives.bed
 done
 
-cat $peaks $outdir/$task/$task.all.positives.bed > $outdir/$task/$task.all.peaks.bed
+cut -f 1-3 $peaks > $outdir/$task/$task.peaks.bed
 
-bedtools intersect -v -a $genomewide_gc -b $peaks > $outdir/$task/$task.candidate.negatives.tsv
+cat $outdir/$task/$task.peaks.bed $outdir/$task/$task.all.positives.bed > $outdir/$task/$task.all.positives.peaks.bed
+
+bedtools intersect -v -a $genomewide_gc -b $outdir/$task/$task.all.positives.peaks.bed > $outdir/$task/$task.candidate.negatives.tsv
 
