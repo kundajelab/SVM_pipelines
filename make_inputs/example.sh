41c73af67987d3f7e970e87d8f34af344f90a575
soumyakundu <soumya.kundu@stanford.edu>
2021-01-11 09:40:39 -0800

    Added chrom sizes and updated example

--- make_inputs/example.sh
+++ make_inputs/example.sh
@@ -2,13 +2,18 @@
 
 outdir=/oak/stanford/groups/akundaje/projects/cad/svm_inputs
 ref_fasta=/oak/stanford/groups/akundaje/refs/mm10/GRCm38.p4.genome.fa
-genomewide_gc=/oak/stanford/groups/akundaje/soumyak/refs/mm10/gc_mm10_nosmooth.tsv
+#genomewide_gc=/oak/stanford/groups/akundaje/soumyak/refs/mm10/gc_mm10_nosmooth.tsv
 genome=mm10
 ntrain=60000
+chrom_sizes=mm10.chrom.sizes
 
 [[ -d $outdir/logs ]] || mkdir $outdir/logs
 
 cd /home/groups/akundaje/soumyak/SVM_pipelines/make_inputs
+
+#make_gc_nosmooth_track --ref_fasta $ref_fasta \
+#                       --chrom_sizes $chrom_sizes \
+#                       --out_prefix $genomewide_gc
 
 for task in c11 c12 c15 c16 c18
 do
